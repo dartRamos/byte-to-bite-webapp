1735a50e95a5847a98baee6162cb9badaed2aca6
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-05-08 19:43:52 -0400

    Created all of schema and seeds for database.

--- backend/db/schema/01_schema.sql
+++ backend/db/schema/01_schema.sql
@@ -6,19 +6,17 @@ DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS user_ingredients CASCADE;
 DROP TABLE IF EXISTS saved_recipes CASCADE;
 DROP TABLE IF EXISTS recipe_matches CASCADE;
-
 -- CREATE TABLES FROM ERD 
 
 CREATE TABLE users (    -- To save user session for saved recipes/pantry
   id SERIAL PRIMARY KEY,
-  created_at DATE -- Will be used for when we add a real login
+  created_at TIMESTAMP  -- Will be used for when we add a real login
 );
 
-CREATE TABLE user_ingredients ( -- Only used for that specific recipe search
+CREATE TABLE user_ingredients ( -- Ingredients used for search
   id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  ingredient_name TEXT NOT NULL,
-  is_pantry_staple BOOLEAN NOT NULL
+  ingredient_name TEXT NOT NULL
 )
 
 CREATE TABLE saved_recipes ( -- To save favorited recipe ID from API 
@@ -26,7 +24,10 @@ CREATE TABLE saved_recipes ( -- To save favorited recipe ID from API
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   recipe_id INTEGER NOT NULL,
   title TEXT NOT NULL, -- To display title of recipe on app
-  image_url TEXT NOT NULL -- To display recipe image on app
+  image_url TEXT NOT NULL, -- To display recipe image on app
+  instructions JSONB, -- To save instructions + cook timers
+  ingredients JSONB, -- To save ingredients + measurements
+  is_favorited BOOLEAN DEFAULT FALSE -- To decide if recipe is saved to DB
 );
 
 CREATE TABLE recipe_matches ( -- Used to check the recipe ingredient match %
