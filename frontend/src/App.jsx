667485f92b710f5d7275c8725792c7088d6ef5bb
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-05-13 20:25:06 -0700

    Route Change - Alter all search form unput to SearchPage Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,25 @@
-import './App.css'
-import  RecipesByIngredients from "./components/RecipesByIngredients"
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import SearchPage from './components/SearchPage';
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-blue-600">Welcome to Byte-to-Bite</h1>
-      <br/>
-      <RecipesByIngredients />
-    </div>
+    <Router>
+      <div className="App">
+        <h1 className="text-3xl font-bold text-blue-600">Welcome to Byte-to-Bite</h1>
+        <br />
+        {/* Button to navigate to the SearchPage */}
+        <Link to="/search">
+          <button className="btn btn-primary">Search Page</button>
+        </Link>
 
-  )
+        {/* Define routes */}
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
