e2e1c26aef9e5a111fceb85a36464924efcb958b
TayrineSoares <tayrinecristina@hotmail.com>
2025-05-15 14:06:58 -0400

    Create Favourite button component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
         <Route path="/search" element={<SearchPage />} />
       </Routes>
       {/* <SearchPage /> */}
-      <div className="bg-red-600 text-yellow-300 font-extrabold text-4xl p-8 rounded-xl shadow-2xl transform rotate-6 hover:scale-110 transition-all duration-500 testing-tailwind">
-       TESTING TAILWIND TESTING TAILWIND TESTING TAILWIND!
+      <div className="bg-red-600 text-yellow-300 p-8 text-4xl">
+      Tailwind should work here!
       </div>
     </>
   );
---
+++ frontend/src/components/FavouriteButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FavouriteButton = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default FavouriteButton;
