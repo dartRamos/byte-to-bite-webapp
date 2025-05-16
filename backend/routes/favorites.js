fd259fb25e0c469686b6e3f53017c5ca63c8003b
TayrineSoares <tayrinecristina@hotmail.com>
2025-05-15 21:30:43 -0400

    fix tailwind issue

--- backend/routes/favorites.js
+++ backend/routes/favorites.js
@@ -56,6 +56,7 @@ router.get('/saved-recipes', async (req, res) => {
   try {
     const userId = 1; // hardcoded for now
     const savedRecipes = await getSavedRecipesByUserId(userId);
+    
     res.json(savedRecipes);
   } catch (err) {
     console.error('Error fetching saved recipes:', err);
--- frontend/src/components/SavedRecipesPage.jsx
+++ frontend/src/components/SavedRecipesPage.jsx
@@ -6,14 +6,22 @@ const SavedRecipesPage = () => {
 
   const [savedRecipes, setSavedRecipes] = useState([]);
 
+  useEffect(() => {
+  axios.get('/api/favorites/saved-recipes')
+    .then((res) => setSavedRecipes(res.data))
+    .catch((err) => console.error("Error loading saved recipes:", err));
+  }, []);
+
 
   return (
     <div className="saved-recipes-page">
       <h1>All Saved Recipes </h1>
       <div className="recipe-grid">
-        {savedRecipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-
+        {savedRecipes.map(({ recipe_id, title, image_url }) => (
+         <RecipeCard
+           key={recipe_id}
+           recipe={{ id: recipe_id, title, image: image_url }}
+        />
         ))}
 
 
