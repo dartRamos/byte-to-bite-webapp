cc5c06938099b954fce71a2bae7230de320e8108
TayrineSoares <tayrinecristina@hotmail.com>
2025-05-17 14:03:30 -0400

    add view full recipe button and triggers modal

--- frontend/src/components/SavedRecipesCard.jsx
+++ frontend/src/components/SavedRecipesCard.jsx
@@ -1,6 +1,34 @@
+import React, {useState} from "react";
 import '../styling/SavedRecipesCard.css'
+import axios from "axios";
+import RecipeModal from "./RecipeModal";
 
 const SavedRecipesCard = ({savedRecipes}) => {
+
+  // store selected Recipe in state so React can re-render 
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
+  // state to control modal
+  const [showModal, setShowModal] = useState(false); 
+
+  const handleViewRecipe = async (recipe) => {
+    try {
+      // Send GET request to backend API, passing recipe ID as a query parameter
+      const response = await axios.get(`http://localhost:8080/api/spoonacular/recipesById?id=${recipe.recipe_id}`);
+
+      // store recipe data in state *AFTER* the call
+      setSelectedRecipe(response.data);
+
+      // triggers modal 
+      setShowModal(true);
+    
+      //console logging the data for debugging
+      console.log("Full recipe data:", response.data)
+      
+    } catch (error) {
+      console.error("Error fetching recipe info:", error.message);
+    }
+     console.log("Modal should show:", showModal);
+  };
 
   return (
     <div className="saved-recipe-container">
@@ -12,10 +40,30 @@ const SavedRecipesCard = ({savedRecipes}) => {
             <div key={recipe.recipe_id} className="recipe-card">
               <h2 className="recipe-title">{recipe.title}</h2>
               <img src={recipe.image_url} alt={recipe.title} className="recipe-image" />
+
+              {/* Add button to trigger full recipe fetch */}
+              {/* Pass the recipe to the handler */}
+              <button type="button" onClick={() => handleViewRecipe(recipe)}>View Full Recipe </button>
+              
             </div>
+            
           ))}
+          
         </div>
       )}
+
+      {/* When showModal is true, and there is a selected Recipe, render modal */}
+      {/* passes function onClose and the full recipe as a prop to RecipeModal component */}
+      {showModal && selectedRecipe && (
+        <RecipeModal 
+          fullRecipe={selectedRecipe}
+          onClose={() => {
+            setShowModal(false); 
+            setSelectedRecipe(null);
+          }} 
+        />
+      )}
+      
     </div>
   )
 };
