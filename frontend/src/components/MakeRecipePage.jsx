2fa762af812af1a561d556b63021bbc5563c4095
Rhaelyn L <rhaelynlashmar@gmail.com>
2025-05-18 21:48:51 -0700

    Feature - Create makeRecipePage

--- frontend/src/components/MakeRecipePage.jsx
+++ frontend/src/components/MakeRecipePage.jsx
@@ -16,48 +16,49 @@ function MakeRecipePage({ onFavoritesChange }) {
   return (
     <div className="make-recipe-page">
       <div className="make-recipe-content">
-
-        {/* Favourite button */}
-        <FavoriteButton 
-          fullRecipe={fullRecipe} 
-          initialFavorited={isFavorite}
-          onFavoritesChange={onFavoritesChange}
-        />
-
-        {/* Title */}
-        <h1 className="make-recipe-title">{fullRecipe.title}</h1>
-
-        {/* Image */}
-        {fullRecipe.image && (
-          <img 
-            src={fullRecipe.image} 
-            alt={fullRecipe.title} 
-            className="make-recipe-image"
+        <div className="make-recipe-main">
+          {/* Favourite button */}
+          <FavoriteButton 
+            fullRecipe={fullRecipe} 
+            initialFavorited={isFavorite}
+            onFavoritesChange={onFavoritesChange}
           />
-        )}
 
-        {/* Time and Servings */}
-        <div className="make-recipe-meta">
-          <p><strong>Ready in:</strong> {fullRecipe.readyInMinutes} minutes</p>
-          <p><strong>Servings:</strong> {fullRecipe.servings}</p>
-        </div>
+          {/* Title */}
+          <h1 className="make-recipe-title">{fullRecipe.title}</h1>
 
-        {/* Ingredients */}
-        {fullRecipe.extendedIngredients?.length > 0 && (
-          <div className="make-recipe-ingredients">
-            <h3 className="make-recipe-section-title">Ingredients</h3>
-            <ul className="make-recipe-ingredient-list">
-              {fullRecipe.extendedIngredients.map((ingredient, index) => (
-                <li key={index}>{ingredient.original}</li>
-              ))}
-            </ul>
+          {/* Image */}
+          {fullRecipe.image && (
+            <img 
+              src={fullRecipe.image} 
+              alt={fullRecipe.title} 
+              className="make-recipe-image"
+            />
+          )}
+
+          {/* Time and Servings */}
+          <div className="make-recipe-meta">
+            <p><strong>Ready in:</strong> {fullRecipe.readyInMinutes} minutes</p>
+            <p><strong>Servings:</strong> {fullRecipe.servings}</p>
           </div>
-        )}
+
+          {/* Ingredients */}
+          {fullRecipe.extendedIngredients?.length > 0 && (
+            <div className="make-recipe-ingredients">
+              <h3 className="make-recipe-section-title">Ingredients</h3>
+              <ul className="make-recipe-ingredient-list">
+                {fullRecipe.extendedIngredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient.original}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
 
         {/* Instructions */}
         {fullRecipe.instructions && (
           <div className="make-recipe-instructions">
-            <h3 className="make-recipe-section-title">Instructions</h3>
+            <h3 className="make-recipe-instructions-title">Instructions</h3>
             <div
               className="make-recipe-instructions-content"
               dangerouslySetInnerHTML={{ __html: fullRecipe.instructions }}
