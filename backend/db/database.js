9e3f4153af5f0aad30ca3997d6911f7117aa47fa
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-05-29 18:41:24 -0400

    fix

--- backend/db/database.js
+++ backend/db/database.js
@@ -8,7 +8,10 @@ const dbParams = {
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 };
 
 const db = new Pool(dbParams);
