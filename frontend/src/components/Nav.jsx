0472b552624f90052d11f1fd2d459a5510a550df
Dartanhan Ramos <alessandroramosd@gmail.com>
2025-07-02 15:35:13 -0400

    removed contact route

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -26,9 +26,6 @@ const Nav = () => {
         <li>
           <NavLink to='/about'>About</NavLink>
         </li>
-        <li>
-          <NavLink to='/contact'>Contact</NavLink>
-        </li>
       </ul>
     </nav>
   )
